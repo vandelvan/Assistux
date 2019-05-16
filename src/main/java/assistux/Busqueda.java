1e0738465a398b95054705f1ae433f12e7a14eed
vandelvan <vandel_van@hotmail.com>
2019-05-16 14:26:50 -0500

    Ahora realiza busquedas en youtube

--- src/main/java/assistux/Busqueda.java
+++ src/main/java/assistux/Busqueda.java
@@ -10,23 +10,36 @@ public class Busqueda {
     	String[] vBuscar = {"busca", "investiga", "busqueda", "indaga",
 				"buscar", "investigar", "indagar"};	//vuelve a declarar el arreglo de strings con las variaciones de busqueda
     	String[] textoSeparado = texto.split(" ");
+    	String ur="";
     	for(int i=0; i < vBuscar.length; i++) {
     		if(textoSeparado[0].equalsIgnoreCase(vBuscar[i]))	
     			textoSeparado[0] = "";	//si encuentra la primer palabra clave, la quita de la busqueda
     		if(textoSeparado.length > 2) {
-    			if(textoSeparado[0].equalsIgnoreCase(vBuscar[i])	   && 
-    				textoSeparado[1].equalsIgnoreCase("en") 	   	   && 
-					textoSeparado[2].equalsIgnoreCase("google")		   ||
-					textoSeparado[2].equalsIgnoreCase("internet")	   ){
-    					textoSeparado[0] = "";
-    					textoSeparado[1] = "";	//quita cualquier palabra clave de la busqueda
-    					textoSeparado[2] = "";
+    			if(textoSeparado[2].equalsIgnoreCase("youtube") ||
+    			   textoSeparado[1].equalsIgnoreCase("youtube")) {
+    				ur =  "https://www.youtube.com/results?search_query=";
+    	    					textoSeparado[0] = "";
+    	    					textoSeparado[1] = "";	//quita cualquier palabra clave de la busqueda
+    	    					textoSeparado[2] = "";
+    	    			}
+    				}
+    			else {
+    				ur = "http://www.google.com/search?q=";
+    				if(textoSeparado.length > 2)
+		    			if(textoSeparado[0].equalsIgnoreCase(vBuscar[i])	   && 
+		    				textoSeparado[1].equalsIgnoreCase("en") 	   	   && 
+							textoSeparado[2].equalsIgnoreCase("google")		   ||
+							textoSeparado[2].equalsIgnoreCase("internet")){
+		    					textoSeparado[0] = "";
+		    					textoSeparado[1] = "";	//quita cualquier palabra clave de la busqueda
+		    					textoSeparado[2] = "";
+		    			}
     			}
     		}
-    	}
 		texto = String.join(" ", textoSeparado);	//acomoda la busqueda sin las palabras clave
+		texto = texto.trim();
     	String busqueda = texto.replace(" ","+");	//separa las palabras con + en vez de espacios
-        String url = "http://www.google.com/search?q="+busqueda;	//Arma la busqueda
+        String url = ur+busqueda;	//Arma la busqueda
 
         if(Desktop.isDesktopSupported()){	//PARA ABRIR EL EXPLORADOR EN SISTEMAS WINDOWS
             Desktop desktop = Desktop.getDesktop();
